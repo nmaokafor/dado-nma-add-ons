e18c05a47cc3281e3f90746108589af6344b45e6
Chidinma Okafor <chidinma.okafor@venturegardengroup.com>
2021-08-28 15:45:02 +0100

    Style navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,59 +1,69 @@
 import Link from "next/link";
 import Image from "next/image";
 import SVGS from "../../public/svgs";
+import styles from "./Navbar.module.scss";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const closeMobileMenu = () => setOpen(false);
+
   return (
     <>
-      <nav>
+      <nav className={styles.navbar}>
         <div>
           {/* Logo */}
 
-          <div>
+          <div className={styles.logoParent}>
             <Link href="/">
-              <a href="#">
-                <Image
-                  src="/assets/images/toplogo.png"
-                  layout="responsive"
-                  objectFit="contain"
-                  height={40}
-                  width={192}
-                  alt="Petlab co logo"
-                />
-              </a>
+              {/* <a href="#"> */}
+              <Image
+                src="/assets/images/toplogo.png"
+                layout="responsive"
+                objectFit="contain"
+                className={styles.logo}
+                height={40}
+                width={192}
+                alt="Petlab co logo"
+              />
+              {/* </a> */}
             </Link>
           </div>
         </div>
         {/* Menu */}
-        <ul>
-          <li>
+        <ul
+          className={
+            open ? `${styles.navmenu} ${styles.active}` : styles.navmenu
+          }
+        >
+          <li className={styles.navitem} onClick={closeMobileMenu}>
             {SVGS.dashboard()}
 
             <a href="#">Dashboard</a>
           </li>
-          <li>
+          <li className={styles.navitem} onClick={closeMobileMenu}>
             {SVGS.mypets()}
 
             <a href="#">My Pets</a>
           </li>
-          <li>
+          <li className={styles.navitem} onClick={closeMobileMenu}>
             {SVGS.subscription()}
             <a href="#">My Subscriptions</a>
           </li>
-          <li>
+          <li className={styles.navitem} onClick={closeMobileMenu}>
             {SVGS.orders()}
 
             <a href="#">Orders</a>
           </li>
-          <li>
+          <li className={styles.navitem} onClick={closeMobileMenu}>
             {SVGS.account()}
             <a href="#">My Account</a>
           </li>
-          <li>
+          <li className={styles.navitem} onClick={closeMobileMenu}>
             {SVGS.addons()}
             <a href="#">Extra Add-Ons</a>
           </li>
-          <li>
+          <li className={styles.navitem} onClick={closeMobileMenu}>
             {SVGS.signout()}
             <a href="#">Signout</a>
           </li>
@@ -62,7 +72,9 @@ const Navbar = () => {
         {/* </div> */}
 
         {/* Hamburger */}
-        <div>{SVGS.hamburger()}</div>
+        <div onClick={() => setOpen(!open)} className={styles.hamburger}>
+          {SVGS.hamburger()}
+        </div>
       </nav>
     </>
   );
